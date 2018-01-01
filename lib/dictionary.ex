077ea1c9820e41ff06b4e98200607b7c10cd2d3c
Ian Vaughan <ian@quiqup.com>
2018-01-01 08:08:40 +0000

    move

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -4,8 +4,9 @@ defmodule Dictionary do
     |> Enum.random
   end
 
-  def word_list(file \\ "assets/words.txt") do
+  def word_list(file \\ "../assets/words.txt") do
     file
+    |> Path.expand(__DIR__)
     |> File.read!
     |> String.split(~r/\n/)
   end
